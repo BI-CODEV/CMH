e1757a7da80369af41d0942d240a371e75fc59fa
BICODEV <85914726+bicodev@users.noreply.github.com>
2021-07-18 02:11:38 +0500

    Update header.php

--- header.php
+++ header.php
@@ -1,1 +1,12 @@
+<!-- Header -->
 
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <title>Casinotemplates.net - Template 9</title>
+    <meta name="Description" content="Your website description goes here.">
+    <meta name="Cassino " content="keyword1, keyword2, keyword3">
+    <link href="style.css" rel="stylesheet" type="text/css">
+</head>
